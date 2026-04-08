bffe03b28843a9a61e6378ddd5fb90399b96cfd4
halaemadali <halaemad177@gmail.com>
2026-04-08 22:51:26 +0200

    Update Main.java

--- oopProject/src/Main.java
+++ oopProject/src/Main.java
@@ -1,5 +1,8 @@
 
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 import java.util.ArrayList;
 
 public class Main {
@@ -10,7 +13,31 @@ public class Main {
         Room r1=new Room();
         r1.addAmenity(wifi);
         System.out.println( r1.getAmenities().get(0));
+        r1.removeAmenity(wifi);
 
+
+        RoomType RT=new RoomType();
+
+        Room r=new Room();
+        r.settype(RT);
+        r.gettype().setBasePrice(2000.650);
+        r.gettype().setCapacity(2);
+        r.gettype().setNO3("single");
+
+        Amenity TV=new Amenity("TV");
+        Amenity bar=new Amenity("bar");
+        r.setroomnumber(102);
+        r.addAmenity(wifi);
+        r.addAmenity(TV);
+        r.setisavailable(true);
+        r.checkavailability(r.getisavailable());
+        r.getAmenities();
+        r.getroomnumber();
+        r.getisavailable();
+        r.removeAmenity(wifi);
+        System.out.println (r.gettype().getBasePrice());
+        System.out.println (r.gettype().getCapacity());
+        System.out.println (r.gettype().getNO3());
 
     }
 }
