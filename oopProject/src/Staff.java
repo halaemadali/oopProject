4f5a73e85b1c2f27d7bb38ac9fab8d7ff9ebc68c
saraelnahass <sara2007elnahass@gmail.com>
2026-04-09 21:37:03 +0200

    Update Staff.java

--- oopProject/src/Staff.java
+++ oopProject/src/Staff.java
@@ -1,1 +1,24 @@
+import java.time.LocalDate;
 
+public abstract class Staff {
+
+    private String username;
+    private String password;
+    private LocalDate dateOfBirth;
+    private Role role;
+    private int workingHours;
+
+      //Constructor
+    public Staff(String username, String password, LocalDate dateOfBirth, Role role, int workingHours) {
+
+        this.username = username;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.role = role;
+        this.workingHours = workingHours;
+      
+    }
+
+  
+
+  
