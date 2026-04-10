e1b385204361d12e70b2ab18d9fd9fd136ca95f8
Mariam <101900968+Mariam12Mohamed@users.noreply.github.com>
2026-04-10 14:44:19 +0200

    Add reservations list to HotelDatabase
    Added a reservations list to the HotelDatabase class.

--- oopProject/src/HotelDatabase.java
+++ oopProject/src/HotelDatabase.java
@@ -1,7 +1,13 @@
+package com.mycompany.main;
+
+import java.util.ArrayList;
+
+
 class HotelDatabase {
 
- public static ArrayList<Guest> guests = new ArrayList<>();
-    public static ArrayList<Room> rooms = new ArrayList<>();
     
-}
+  public static ArrayList<Guest> guests = new ArrayList<>();
+    public static ArrayList<Room> rooms = new ArrayList<>();
+      public static ArrayList<Reservation> reservations = new ArrayList<>();
 
+}
