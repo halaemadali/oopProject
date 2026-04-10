2b13e5a2cffa75c3198a40d666b292dd46ddb093
saraelnahass <sara2007elnahass@gmail.com>
2026-04-10 19:00:22 +0200

    Update Receptionist.java

--- oopProject/src/Receptionist.java
+++ oopProject/src/Receptionist.java
@@ -8,13 +8,49 @@ public class Receptionist extends Staff {
     }
 
 
-    public void checkIn(){
-      
+     public void checkIn(int reservationID ){
+        for (Reservation r : HotelDatabase.reservations) {
+
+            if (r.getID() == reservationID) {
+
+                if (r.getStatus() == ReservationStatus.CONFIRMED) {
+                    System.out.println("Already checked in");
+                    return;
+                }
+
+                r.confirm();
+
+                System.out.println("Guest checked in");
+                return;
+            }
+        }
+
+        System.out.println("Reservation not found");
     }
 
   
-    public void checkOut(){
 
+    public void checkOut(int reservationID){
+        for (Reservation r : HotelDatabase.reservations) {
+
+            if (r.getID() == reservationID) {
+
+                if (r.getStatus() == ReservationStatus.COMPLETED) {
+                    System.out.println("Already checked out");
+                    return;
+                }
+
+                r.complete();
+
+                System.out.println("Guest checked out");
+                return;
+            }
+        }
+
+        System.out.println("Reservation not found");
     }
+
+
+
 
 }
