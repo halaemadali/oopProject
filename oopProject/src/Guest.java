80e7545ed3daf640fe16a9de7ec40f404500b512
halaemadali <halaemad177@gmail.com>
2026-04-18 00:45:43 +0200

    Update Guest.java

--- oopProject/src/Guest.java
+++ oopProject/src/Guest.java
@@ -1,5 +1,4 @@
 
-package com.mycompany.main;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -8,7 +7,7 @@ import java.util.List;
 
 public class Guest {
 
-// Variables
+    // Variables
     private String username;
     private String password;
     private LocalDate dateOfBirth;
@@ -17,23 +16,23 @@ public class Guest {
     private Gender gender;
 
     private List<RoomType> roomPreferences;
-    
+
     private List<Room> availableRooms;
-private List<Reservation> reservations ;
+    private List<Reservation> reservations ;
 
 
-   //Constructor
-  public Guest() {
-    this.availableRooms = new ArrayList<>();
-    this.reservations = new ArrayList<>();
-}
+    //Constructor
+    public Guest() {
+        this.availableRooms = new ArrayList<>();
+        this.reservations = new ArrayList<>();
+    }
 
-   
+
     public Guest(String username, String password, LocalDate dateOfBirth,
                  double balance, String address, Gender gender,
                  List<RoomType> roomPreferences)throws InvalidUsernameException , Exception  {
 
-         setUsername(username);
+        setUsername(username);
         setPassword(password);//set for validation
         setDateOfBirth(dateOfBirth);
         setBalance(balance);
@@ -46,58 +45,58 @@ private List<Reservation> reservations ;
 
 
 
-  
-   // Getters & Setters 
+
+    // Getters & Setters
 
     public String getUsername() {
         return username;
     }
 
-  
 
 
-public void setUsername(String username) throws InvalidUsernameException {
 
-    if (username == null) {
-        throw new InvalidUsernameException("Username cannot be null");
-    } 
-    else if (username.trim().isEmpty()) {
-        throw new InvalidUsernameException("Username cannot be empty");
-    } 
-    else {
-        this.username = username;
+    public void setUsername(String username) throws InvalidUsernameException {
+
+        if (username == null) {
+            throw new InvalidUsernameException("Username cannot be null");
+        }
+        else if (username.trim().isEmpty()) {
+            throw new InvalidUsernameException("Username cannot be empty");
+        }
+        else {
+            this.username = username;
+        }
     }
-}
 
-  
 
-  
+
+
     public String getPassword() {
         return password;
     }
 
-  public void setPassword(String password) throws Exception {
-    if (password == null) {
-        throw new Exception("Password cannot be null");
-    } 
-    else if (password.length() < 6) {
-        throw new Exception("Password must be at least 6 characters");
-    } 
-    else {
-        this.password = password;
+    public void setPassword(String password) throws Exception {
+        if (password == null) {
+            throw new Exception("Password cannot be null");
+        }
+        else if (password.length() < 6) {
+            throw new Exception("Password must be at least 6 characters");
+        }
+        else {
+            this.password = password;
+        }
     }
-}
 
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
     public void setDateOfBirth(LocalDate dateOfBirth) {
-   
-      if (dateOfBirth == null) {
-          System.out.println("Date Of Birth is null");
+
+        if (dateOfBirth == null) {
+            System.out.println("Date Of Birth is null");
         }
-     else{ this.dateOfBirth = dateOfBirth;}
+        else{ this.dateOfBirth = dateOfBirth;}
     }
 
     public double getBalance() {
@@ -106,120 +105,119 @@ public void setUsername(String username) throws InvalidUsernameException {
 
     public void setBalance(double balance) {
         if (balance < 0) {
-          System.out.println("Balance cannot be negative");
-         
+            System.out.println("Balance cannot be negative");
+
         }
-      else{  this.balance = balance;}
+        else{  this.balance = balance;}
     }
 
     public List<Reservation> getReservations() {
         return reservations;
     }
 
- //methodes
+    //methodes
 
-       //register
-    
+    //register
+
     public void register(){
-    
-    for (Guest g : HotelDatabase.guests) {
-        
-        if(g.getUsername().equals(this.username)){
-          System.out.println("Username already exists!");
+
+        for (Guest g : HotelDatabase.guests) {
+
+            if(g.getUsername().equals(this.username)){
+                System.out.println("Username already exists!");
                 return;
+            }
         }
+        HotelDatabase.guests.add(this);
+        System.out.println("Registered successfully!");
     }
-      HotelDatabase.guests.add(this);
-      System.out.println("Registered successfully!");
-    }
-    
-       //login
-    
-   public static Guest login(String username, String password){
-    for (Guest g : HotelDatabase.guests) {
-        if (g.getUsername().equals(username) &&
-                g.getPassword().equals(password)) {
+
+    //login
+
+    public static Guest login(String username, String password){
+        for (Guest g : HotelDatabase.guests) {
+            if (g.getUsername().equals(username) &&
+                    g.getPassword().equals(password)) {
 
                 System.out.println("Login successful!");
                 return g;
             }
-     
-     }
-    
-    System.out.println("Invalid username or password!");
-    return null;
-    
-    }
-    
-    
-    
-       //View Available Rooms
-  public List<Room> viewAvailableRooms(){
-        availableRooms.clear();
-    for (Room room : HotelDatabase.rooms) {
-    if ((roomPreferences == null || roomPreferences.contains(room.gettype()))
-        && room.getisavailable()) {
-        
-        availableRooms.add(room);
-    }
-}
 
-    return availableRooms;
-       
-   }
+        }
+
+        System.out.println("Invalid username or password!");
+        return null;
+
+    }
+
+
+
+    //View Available Rooms
+    public List<Room> viewAvailableRooms(){
+        availableRooms.clear();
+        for (Room room : HotelDatabase.rooms) {
+            if ((roomPreferences == null || roomPreferences.contains(room.getType()))
+                    && room.getisavailable()) {
+
+                availableRooms.add(room);
+            }
+        }
+
+        return availableRooms;
+
+    }
 
     //make Reservation
     public void makeReservation(Reservation r){
-    
-    if (r==null){
-     System.out.println("Invalid reservation");
-        return; 
-    }
-    reservations.add(r);//list of the guest
-    HotelDatabase.reservations.add(r);//global
-     System.out.println("Reservation added successfully!");
-    
-    }
 
-    
-//cancle reservation
-public void cancelReservation(Reservation r) {
+        if (r==null){
+            System.out.println("Invalid reservation");
+            return;
+        }
+        reservations.add(r);//list of the guest
+        HotelDatabase.reservations.add(r);//global
+        System.out.println("Reservation added successfully!");
 
-    if (r == null) {
-        System.out.println("Invalid reservation");
-        return;
     }
 
-    r.cancel();
+
+    //cancle reservation
+    public void cancelReservation(Reservation r) {
+
+        if (r == null) {
+            System.out.println("Invalid reservation");
+            return;
+        }
+
+        r.cancel();
+    }
+
+    //view reservation
+    public void viewReservations() {
+
+        for (Reservation r : reservations) {
+            System.out.println("Room: " + r.getRoom().getRoomNumber());
+        }
+    }
+
+    //checkout
+    public void checkout() {
+
+        for (Reservation r : reservations) {
+            r.complete();
+        }
+    }
+
 }
 
-//view reservation
-public void viewReservations() {
-
-    for (Reservation r : reservations) {
-        System.out.println("Room: " + r.getRoom().getroomnumber());
-    }
-}
-
-//checkout
-public void checkout() {
-
-    for (Reservation r : reservations) {
-        r.complete();
-    }
-}
-    
-  }
-  
 
 
-    
 
 
-    
-  
-  
 
 
- 
+
+
+
+
 
