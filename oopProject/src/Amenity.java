59221ffe0255b888f2f37b2686a277cb722dd0d1
saraelnahass <sara2007elnahass@gmail.com>
2026-04-21 03:34:45 +0200

    Update Amenity.java

--- oopProject/src/Amenity.java
+++ oopProject/src/Amenity.java
@@ -35,4 +35,11 @@ public class Amenity {
         return getName();
     }
 
+    @Override
+    public String toString(){
+        return    "( " + name +
+                " price: "  + price + " )";
+
+    }
+
 }
